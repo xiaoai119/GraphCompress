a69ad57588c3c52c24ad62872a4a246a4f0313d2
xfj <372994102@qq.com>
2017-09-23 13:51:26 +0800

    first commit

---
+++ src/main/java/com/xfj/lab/App.java
@@ -0,0 +1,13 @@
+package com.xfj.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
